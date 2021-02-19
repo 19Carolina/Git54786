c1475c1e0a733eb0f5a7ee34297f5b3161b004ee
carol <carol@192.168.1.71>
2021-02-18 20:30:04 -0600

    Mi primer commit

---
+++ src/git54786/Git54786.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git54786;
+
+/**
+ *
+ * @author carol
+ */
+public class Git54786 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nombre = "Carolina Colli";
+        String matri = "54786";
+        
+      System.out.println("Soy " + nombre + " y mi matricula es: " + matri);
+       
+        
+        
+        //acabo de crear mi repositorio locol en team/Initialize repository
+        
+        int edad = 20; 
+        int sueldo = 1000; 
+    }
+    
+}
